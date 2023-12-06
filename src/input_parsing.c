ffbae566bd0b5aa3905fe2cc750ff72eaa72f228
Benjamin Plante <benplante99@gmail.com>
2023-12-06 18:05:02 -0500

    temp

---
+++ src/input_parsing.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_parsing.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bplante <bplante@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 14:21:18 by bplante           #+#    #+#             */
+/*   Updated: 2023/12/06 15:07:36 by bplante          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static int	is_num(char *str);
+
+t_cstack	*parse_inputs(char **args)
+{
+	t_cstack	*cstack;
+	int			num;
+
+	cstack = NULL;
+	while (args)
+	{
+		if (is_num(args[0]))
+			cstack = cstack_add(cstack, ft_atoi(args[0]));
+		else
+		{
+			cstack_clear(cstack);
+			ft_printf_fd("Error\n", 2);
+			return (NULL);
+		}
+		args++;
+	}
+	return (cstack);
+}
+
+int	is_num(char *str)
+{
+	while (str[0])
+	{
+		if (!ft_isdigit(str[0]))
+			return (false);
+		str++;
+	}
+	return (true);
+}
